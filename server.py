91e00934772581ab03b48c6386420f500ce03daa
Danyal Raza <Danyal@v1022-rn-111-195.campus-dynamic.uwaterloo.ca>
2016-01-17 22:39:08 -0500

    add server.py

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from flask import jsonify
+import requests
 from flask import render_template
 from riot_client import Client
+from key import key
 
 app = Flask(__name__)
 
+riotapi = Client(key)
+
 @app.route("/")
 def home():
-    return render_template('layout.html', name=name)
+    return '<h1>Using Riot API</h1>'
+
+@app.route("/summoner/<region>/<username>")
+def champions(username, region):
+    user = username
+    summary = riotapi.get_summoner_by_name(username, region)
+
+    return jsonify(summary)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
